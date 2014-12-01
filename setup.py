ff34b3c6c5869aade8b7228706bc289909862820
Andrew Smalley <andrew@csv2.co.uk>
2014-12-01 14:02:35 +0000

    updated arch distro

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ def getDistro():
     # openSUSE and SLE the same
     if distro.find('suse') != -1:
         distro = 'suse'
+    if distro.find('arch') != -1:
+        distro = 'arch'
     if (distro.find('fedora') != -1
     or distro.find('red hat') != -1
     or distro.find('centos') != -1
